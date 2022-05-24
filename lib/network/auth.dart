224fb2c0528b17905ce637ff165877f4de27445a
Aravind Chowdary <kamaravichow@gmail.com>
2022-05-24 09:39:35 +0530

    auth.dart

---
+++ lib/network/auth.dart
@@ -0,0 +1,4 @@
+const String SENTRY_URL = "https://notnessessary.com/";
+const BASE_URL = "https://api.medito.app/";
+const INIT_TOKEN = "candle_wisplike_unlocking_uncross_wrongness_sludge_halves_ellipse";
+const CONTENT_TOKEN ="wizard_amount_peso_dayroom_jovial_deceiver_detonate_napped";
